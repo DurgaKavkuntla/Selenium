6989335b0f2beec9d25e0e449cf524312f5fc5de
durgakavkuntla <DP@LAPTOP-258E1LQH>
2017-12-15 02:04:09 +0530

    adding redbus calendar code with js- found hidden element issue

--- src/prac/RedBusCalendar.java
+++ src/prac/RedBusCalendar.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -32,33 +33,62 @@ System.setProperty("webdriver.chrome.driver", "C:\\Users\\DP\\Downloads\\chromed
 		
 		driver.get("https://www.redbus.in");
 		
-		driver.findElement(By.xpath("//label[contains(text(),'FROM')]")).click();
+		JavascriptExecutor js= ((JavascriptExecutor) driver);
 		
-		driver.findElement(By.xpath("//label[contains(text(),'FROM')]")).sendKeys("Hyderabad");
+//		WebElement eleHyd= driver.findElement(By.xpath("//label[contains(text(),'FROM')]"));
+//		
+//		WebElement eleVij= driver.findElement(By.xpath("//label[contains(text(),'TO')]"));
+//		
+//		js.executeScript("document.querySelector("+"arguments[0].querySelector('input#src+label.db').innerHTML="+"HYDERBAD",eleHyd);
+//		
+//		js.executeScript("document.querySelector("+"arguments[0].querySelector('input#dest+label.db').innerHTML="+"HYDERBAD",eleVij);
+//		
 		
-		driver.findElement(By.xpath("//label[contains(text(),'FROM')]")).click();
-		
-		driver.findElement(By.xpath("//label[contains(text(),'FROM')]")).sendKeys("Vijayawada");
-		
-		driver.findElement(By.xpath("//label[contains(text(),'Onward Date')]")).click();
-		
-		SimpleDateFormat dt  = new SimpleDateFormat("ddMMMYYYY");
+		SimpleDateFormat dt  = new SimpleDateFormat("dd-MMM-YYYY");
 		
 		String format= dt.format(new Date("1/1/2018"));
+		System.out.println("formated fromdate is "+format);
+	WebElement eleFromDate=driver.findElement(By.xpath("//label[contains(text(),'Onward Date')]"));              
+		//js.executeScript("arguments[0].setAttribute('data-caleng','"+format+"');",eleFromDate);
+//		List<WebElement> list =driver.findElements(By.xpath("//ul[@role='listbox']/descendant::div[@class='sbqs_c']"));
+//		
+//		for(int i =0;i<list.size();i++){
+//			System.out.println(list.get(i).getText());
+////			if(list.get(i).getText().contains("testing tools")){
+////				
+////				list.get(i).click();
+////				break;
+//			
+		eleFromDate.click();
 		
-		List<WebElement> list =driver.findElements(By.xpath("//ul[@role='listbox']/descendant::div[@class='sbqs_c']"));
-		
-		for(int i =0;i<list.size();i++){
-			System.out.println(list.get(i).getText());
-//			if(list.get(i).getText().contains("testing tools")){
-//				
-//				list.get(i).click();
-//				break;
-			
-		}
-		
-		driver.quit();
-		
-		
+String monyear = format.split("-")[1]+" "+format.split("-")[2];
+
+String date =format.split("-")[0];
+
+System.out.println("monyear-->"+monyear+"\n"+"date---->"+date);
+while(true){
+	try{
+	driver.findElement(By.xpath("//td[@class='monthTitle' and text()='"+monyear+"']")).isDisplayed();
+	
+	driver.findElement(By.xpath("//td[@class='wd day' and text()='"+date+"']")).click();
 	}
+	
+	catch(Exception e){
+		List<WebElement> elements=driver.findElements(By.xpath("//td[@class='next']/button"));
+		
+		for(int i=0;i<elements.size();i++){
+			int x=elements.get(i).getLocation().getX();
+			System.out.println(x);
+			if(x!=0){
+				elements.get(i).click();
+			}
+		}
+	}
+	
+	
 }
+		//driver.quit();
+		
+		
+	
+}}
